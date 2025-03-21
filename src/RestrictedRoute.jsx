367c7e072c79086fea7d1fa6f12d447653bbba63
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-03-21 23:40:20 +0100

    feat: :art: routes in App + restricted | private + sharedLayout

---
+++ src/RestrictedRoute.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+const RestrictedRoute = ({ children, redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return isLoggedIn ? children : <Navigate to={redirectTo} />;
+};
+
+export default RestrictedRoute;
---
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TrackerPage = () => {
+  return <div>TrackerPage</div>;
+};
