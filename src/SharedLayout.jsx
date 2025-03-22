3826c24b91e1fd9de230173411619c8eec1297b1
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-03-22 16:46:18 +0100

    feat: :poop: disable PrivateRoute & RestrictedRoute

--- src/SharedLayout.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SharedLayout = () => {
-  return <div>SharedLayout</div>;
-};
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import './index.css';
 import App from './App.jsx';
+import 'modern-normalize';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -9,12 +10,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-      <PersistGate>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
   </StrictMode>,
 );
